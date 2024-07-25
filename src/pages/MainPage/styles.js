a0390ee6f868eb73e9087e922ad747885acc1071
fmancebo <f.92xavier1@gmail.com>
2024-07-25 14:11:14 -0300

    ajuste no justfy

--- src/pages/MainPage/styles.js
+++ src/pages/MainPage/styles.js
@@ -6,6 +6,7 @@ export const Container = styled.div`
   height: 100vh;
   width: 100%;
   flex-direction: column;
+  justify-content: space-evenly;
   align-items: center;
   background-color: ${({ theme }) => theme.colors.gray800};
 
